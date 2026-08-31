2c933c41b308e05a0d02ccec8a040d87f9222095
Wejdan Hatim <wej.hatim@gmail.com>
2026-08-31 12:29:21 +0300

    MySQL_workbench

---
+++ MySQL_Workbench.sql
@@ -0,0 +1,15 @@
+#USE blogs_app;
+/*INSERT INTO user (Name, Email, Password) 
+VALUES 
+('Wejdan Hatim', 'wejdan@email.com', 'pass123');
+INSERT INTO user (Name, Email, Password) 
+VALUES 
+('Mohammad Malhas ', 'mohammad@email.com', 'pass456');*/
+
+#select * from user;
+
+#UPDATE user SET Email = 'wej@email.com' WHERE UserID = 1;
+#select * from user;
+
+#DELETE FROM user WHERE UserID = 2;
+select * from user;
